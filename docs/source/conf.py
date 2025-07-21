77e06baf9a331bd526653416bcea7f2e2a684011
khyeonmin <lamb122@pusan.ac.kr>
2025-07-21 09:39:45 +0900

    docs: switch rst file to md file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,21 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'ko'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
---
+++ docs/source/index.md
@@ -0,0 +1,9 @@
+# stLENS Documentation
+
+Welcome to the stLENS documentation!
+
+
+## Contents
+
+- [Getting Started](getting_started.md)
+- [Installation](installation.md)
